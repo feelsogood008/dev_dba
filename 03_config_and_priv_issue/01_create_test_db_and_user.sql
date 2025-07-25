55cab8545667a8d533fe57f9b907fe044e3bdbab
008 <39646894+feelsogood008@users.noreply.github.com>
2025-07-25 22:04:26 +0900

    Update 01_create_test_db_and_user.sql

--- 03_config_and_priv_issue/01_create_test_db_and_user.sql
+++ 03_config_and_priv_issue/01_create_test_db_and_user.sql
@@ -1,5 +1,5 @@
 -- 테스트용 DB 및 서비스 계정 생성
-CREATE DATABASE demo_db;
+CREATE DATABASE IF NOT EXISTS demo_db DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE USER 'svc_api'@'localhost' IDENTIFIED BY 'svc_pass';
 

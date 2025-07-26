79aa02c9f265dbe0755ca2b411357b9064d89fcf
008 <39646894+feelsogood008@users.noreply.github.com>
2025-07-26 15:10:50 +0900

    Update 05_fix_privileges_and_config.sql

--- 03_config_and_priv_issue/05_fix_privileges_and_config.sql
+++ 03_config_and_priv_issue/05_fix_privileges_and_config.sql
@@ -5,11 +5,4 @@ FLUSH PRIVILEGES;
 -- 재확인
 SHOW GRANTS FOR 'svc_api'@'%';
 
--- legacy_users 테이블 접근 (성공 예상)
-SELECT * FROM legacy_users;
 
--- new_payments 테이블 접근 (성공 예상)
-SELECT * FROM new_payments;
-
--- recent_payments 뷰 접근 (성공 예상)
-SELECT * FROM recent_payments;

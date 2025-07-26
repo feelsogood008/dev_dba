4bf5278babfbc0ed1fbde026875a48ce1aa09fca
008 <39646894+feelsogood008@users.noreply.github.com>
2025-07-26 21:13:31 +0900

    Update 04_run_fast_query.sql

--- 04_index_perf_issue/04_run_fast_query.sql
+++ 04_index_perf_issue/04_run_fast_query.sql
@@ -1,7 +1,8 @@
 USE demo_db;
 
 -- 인덱스 적용 후 동일한 쿼리 실행
-EXPLAIN SELECT post_id, title, created_at
+EXPLAIN ANALYZE 
+SELECT post_id, title, created_at
 FROM board_posts
 WHERE category_id = 3
 ORDER BY created_at DESC

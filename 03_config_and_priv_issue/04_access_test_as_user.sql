70be5413831bb8b7f2bbcf6ae5f39791cf3230f4
008 <39646894+feelsogood008@users.noreply.github.com>
2025-07-26 15:20:26 +0900

    Update 04_access_test_as_user.sql

--- 03_config_and_priv_issue/04_access_test_as_user.sql
+++ 03_config_and_priv_issue/04_access_test_as_user.sql
@@ -1,14 +1,14 @@
 -- svc_api 계정으로 실행 시도
 USE demo_db;
 
--- legacy_users 테이블 접근 (성공 예상)
+-- legacy_users 테이블 접근 (성공)
 SELECT * FROM legacy_users;
 
--- new_payments 테이블 접근 (실패 예상)
+-- new_payments 테이블 접근 (권한이 없으면 실패)
 SELECT * FROM new_payments;
 
--- recent_payments 뷰 접근 (실패 예상)
+-- recent_payments 뷰 접근 (권한이 없으면 실패)
 SELECT * FROM recent_payments;
 
--- 저장 프로시저 호출 (실패 예상)
+-- 저장 프로시저 호출 (권한이 없으면 실패)
 CALL get_user_email(1);

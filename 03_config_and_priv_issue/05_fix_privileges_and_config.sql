6aa0736e524d75766f7907ee568577abd6b1e1e2
008 <39646894+feelsogood008@users.noreply.github.com>
2025-07-26 14:22:12 +0900

    Update 05_fix_privileges_and_config.sql

--- 03_config_and_priv_issue/05_fix_privileges_and_config.sql
+++ 03_config_and_priv_issue/05_fix_privileges_and_config.sql
@@ -1,6 +1,6 @@
 -- 운영 서버 권한 누락 복구
-GRANT SELECT, EXECUTE ON demo_db.* TO 'svc_api'@'localhost';
+GRANT SELECT, EXECUTE ON demo_db.* TO 'svc_api'@'%';
 FLUSH PRIVILEGES;
 
 -- 재확인
-SHOW GRANTS FOR 'svc_api'@'localhost';
+SHOW GRANTS FOR 'svc_api'@'%';

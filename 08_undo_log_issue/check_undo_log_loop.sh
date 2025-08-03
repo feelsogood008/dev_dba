707d899df425c8ff8dbbd705b74d74eee7c7418e
008 <39646894+feelsogood008@users.noreply.github.com>
2025-08-03 14:08:16 +0900

    Update check_undo_log_loop.sh

--- 08_undo_log_issue/check_undo_log_loop.sh
+++ 08_undo_log_issue/check_undo_log_loop.sh
@@ -5,7 +5,7 @@ echo "[undo 로그 상태를 1초 간격으로 30회 확인합니다.]"
 for i in {1..30}
 do
   echo "[$i 회차]"
-  mysql -u root -p -e "
+  mysql -u root -pP@ssw0rd -e "
     SELECT NAME, STATE, TRX_ID, UNDO_NO 
     FROM information_schema.innodb_trx 
     WHERE TRX_STARTED IS NOT NULL;

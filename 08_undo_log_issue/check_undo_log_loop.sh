e2e3c0dbdab27f4fc4fa887ef006ac14358a2760
008 <39646894+feelsogood008@users.noreply.github.com>
2025-08-03 14:13:13 +0900

    Update check_undo_log_loop.sh

--- 08_undo_log_issue/check_undo_log_loop.sh
+++ 08_undo_log_issue/check_undo_log_loop.sh
@@ -6,9 +6,22 @@ for i in {1..30}
 do
   echo "[$i 회차]"
   mysql -u root -pP@ssw0rd -e "
-    SELECT NAME, STATE, TRX_ID, UNDO_NO 
-    FROM information_schema.innodb_trx 
-    WHERE TRX_STARTED IS NOT NULL;
+      SELECT 
+          trx.trx_id AS trx_id,
+          trx.trx_started AS trx_start_time,
+          TIMESTAMPDIFF(SECOND, trx.trx_started, NOW()) AS trx_duration_sec,
+          trx.trx_state,
+          trx.trx_query AS last_query,
+          p.id AS process_id,
+          p.user AS user,
+          p.host AS host,
+          p.command,
+          p.time AS thread_time,
+          p.info AS current_sql
+      FROM information_schema.innodb_trx trx
+      JOIN information_schema.processlist p
+          ON trx.trx_mysql_thread_id = p.id
+      ORDER BY trx_duration_sec DESC;
   "
   sleep 1
 done

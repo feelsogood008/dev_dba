3dbe49fe04104d712a3cfbc30d3ac28784cf0178
008 <39646894+feelsogood008@users.noreply.github.com>
2025-08-06 21:14:23 +0900

    Update 04_delete_many_rows.sql

--- 08_undo_log_issue/04_delete_many_rows.sql
+++ 08_undo_log_issue/04_delete_many_rows.sql
@@ -1,6 +1,9 @@
 USE demo_db;
 
+-- 트랜잭션 내에서 대량 삭제를 수행하여 undo log를 발생시킴
+START TRANSACTION;
 
--- 대량 삭제 (undo log가 비대해지는 원인)
-DELETE FROM user_activity
-WHERE user_id < 50000;
+DELETE FROM undo_test;
+
+-- COMMIT 지연시켜 undo log가 계속 남도록
+-- COMMIT;

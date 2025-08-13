48aa41c9235209ef7aada6ad1da2f3968ad3ba50
008 <39646894+feelsogood008@users.noreply.github.com>
2025-08-13 21:28:41 +0900

    Update 01_enable_slow_query.sql

--- 10_slow_query_analysis/01_enable_slow_query.sql
+++ 10_slow_query_analysis/01_enable_slow_query.sql
@@ -6,7 +6,7 @@ SET GLOBAL slow_query_log = 'ON';
 SET GLOBAL slow_query_log_file = '/var/lib/mysql/mysql-slow.log';
 
 -- 0.5초 이상 걸린 쿼리부터 기록
-SET GLOBAL long_query_time = 0.5;
+SET GLOBAL long_query_time = 1;
 
 -- 슬로우 로그에 비인덱스 SELECT도 포함
 SET GLOBAL log_queries_not_using_indexes = 1;
@@ -15,4 +15,5 @@ SET GLOBAL log_queries_not_using_indexes = 1;
 SHOW VARIABLES LIKE '%slow_query%';
 SHOW VARIABLES LIKE '%long_query_time%';
 SHOW VARIABLES LIKE '%log_queries_not_using_indexes%';
+
 

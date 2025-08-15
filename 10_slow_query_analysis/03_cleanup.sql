7ac671ae244970eb2399c329f44bbe262d2a22ed
008 <39646894+feelsogood008@users.noreply.github.com>
2025-08-15 14:42:26 +0900

    Update 03_cleanup.sql

--- 10_slow_query_analysis/03_cleanup.sql
+++ 10_slow_query_analysis/03_cleanup.sql
@@ -3,8 +3,11 @@ SET GLOBAL slow_query_log = 'OFF';
 USE demo_db;
 
 DROP TABLE IF EXISTS slow_test;
+DROP TABLE IF EXISTS large_table;
+DROP TABLE IF EXISTS lt_users;
 
 DROP DATABASE demo_db;
+
 
 
 

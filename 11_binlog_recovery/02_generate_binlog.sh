764e80cea58918047f47878725c471d1a960225f
008 <39646894+feelsogood008@users.noreply.github.com>
2025-08-17 14:11:22 +0900

    Update 02_generate_binlog.sh

--- 11_binlog_recovery/02_generate_binlog.sh
+++ 11_binlog_recovery/02_generate_binlog.sh
@@ -21,5 +21,9 @@ date "+%Y-%m-%d %H:%M:%S"
 echo "1분간 대기....."
 sleep 60
 
+
+echo "업데이트 전 시간"
+date "+%Y-%m-%d %H:%M:%S"
+
 echo "[3] binlog 생성용 업데이트 실행"
 mysql -u$MYSQL_USER -p$MYSQL_PWD demo_db -e "UPDATE customers SET email = CONCAT(email, '.kr') WHERE id <= 50;"

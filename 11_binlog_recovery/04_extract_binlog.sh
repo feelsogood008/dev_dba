92c31f3265cca4048dc9bf3a40751cd0abe9f386
008 <39646894+feelsogood008@users.noreply.github.com>
2025-08-17 15:03:16 +0900

    Update 04_extract_binlog.sh

--- 11_binlog_recovery/04_extract_binlog.sh
+++ 11_binlog_recovery/04_extract_binlog.sh
@@ -20,7 +20,8 @@ mysqlbinlog \
 
 echo "[2] DELETE 필터링"
 awk 'BEGIN{skip=0}
-     /DELETE /{skip=1}    
+     /DELETE /{skip=1}
+     /CREATE TABLE/{skip=1}     
      /^# at/{skip=0}
      skip==0 {print}' $RAW_SQL > $FILTERED_SQL
 

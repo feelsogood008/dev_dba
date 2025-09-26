406f20f859edc2ac3c52232f991fb316750303bf
008 <39646894+feelsogood008@users.noreply.github.com>
2025-09-26 21:15:52 +0900

    Update run_all.sh

--- 03_config_and_priv_issue/run_all.sh
+++ 03_config_and_priv_issue/run_all.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 # 3장 실습: 권한 및 설정 누락 문제 재현 자동 실행 스크립트
 
+MYSQL_CMD="mysql -u root -pP@ssw0rd"
+
 echo "[1단계] 테스트 DB 및 서비스 계정 생성"
-mysql -u root -p < 01_create_test_db_and_user.sql 
+$MYSQL_CMD < 01_create_test_db_and_user.sql 
 
 echo "[2단계] 테이블, 뷰, 프로시저 생성 (관리자)"
-mysql -u root -p demo_db < 02_create_objects_as_admin.sql
+$MYSQL_CMD < 02_create_objects_as_admin.sql
 
 echo "[3단계] 현재 계정 권한 확인"
-mysql -u root -p < 03_check_user_privileges.sql
+$MYSQL_CMD < 03_check_user_privileges.sql
 
 echo "[4단계] svc_api 서비스 계정으로 접근 테스트, new_payments 테이블 접근 실패"
 mysql -u svc_api -p'P@ssw0rd' demo_db < 04_access_test_as_user.sql
 
 echo "[5단계] 권한 보완 및 재확인"
-mysql -u root -p < 05_fix_privileges_and_config.sql
+$MYSQL_CMD < 05_fix_privileges_and_config.sql
 
 echo "[5-1단계] svc_api 서비스 계정으로 다시 접근 테스트"
 mysql -u svc_api -p'P@ssw0rd' demo_db < 04_access_test_as_user.sql
 
 echo "[6단계] 정리"
-mysql -u root -p < 06_cleanup.sql
+$MYSQL_CMD < 06_cleanup.sql
+

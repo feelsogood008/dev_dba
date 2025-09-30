bf8eeabc3d8e203b0e38166d7ab5463dcb77e012
008 <39646894+feelsogood008@users.noreply.github.com>
2025-09-30 16:48:22 +0900

    Update run_all.sh

--- 04_index_perf_issue/run_all.sh
+++ 04_index_perf_issue/run_all.sh
@@ -8,13 +8,13 @@ echo "[1단계] 테이블 생성 및 데이터 입력"
 $MYSQL_CMD < 01_init_and_data.sql
 
 echo "[2단계] 인덱스 없이 느린 쿼리 실행"
-$MYSQL_CMD demo_db < 02_run_slow_query.sql
+$MYSQL_CMD < 02_run_slow_query.sql
 
 echo "[3단계] 인덱스 생성"
-$MYSQL_CMD demo_db < 03_create_index.sql
+$MYSQL_CMD < 03_create_index.sql
 
 echo "[4단계] 인덱스 적용 후 빠른 쿼리 실행"
-$MYSQL_CMD demo_db < 04_run_fast_query.sql
+$MYSQL_CMD < 04_run_fast_query.sql
 
 echo "[5단계] 정리"
-$MYSQL_CMD demo_db < 05_cleanup.sql
+$MYSQL_CMD < 05_cleanup.sql

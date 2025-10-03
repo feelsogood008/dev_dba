25fda1f41d9dd5b540b4cf0f25a520a09df62f88
008 <39646894+feelsogood008@users.noreply.github.com>
2025-10-03 15:37:25 +0900

    Update 02_check_disk_usage.sh

--- 06_disk_space_issue/02_check_disk_usage.sh
+++ 06_disk_space_issue/02_check_disk_usage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # 디스크 사용량 확인 스크립트
 
-echo "[🔍] MySQL 데이터 디렉토리 사용량 확인:"
+echo "[🔍] MySQL 데이터 디렉토리 사용량 확인"
 sudo du -sh /var/lib/mysql
 
-echo "[🔍] 현재 디스크 사용 현황:"
+echo "[🔍] 현재 디스크 사용 현황"
 df -h

d1140cac602aa36f3e72d928501e1b1cbb9a5e50
008 <39646894+feelsogood008@users.noreply.github.com>
2025-10-04 13:30:26 +0900

    Update run_all.sh

--- 10_slow_query_analysis/run_all.sh
+++ 10_slow_query_analysis/run_all.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 MYSQL_CMD="mysql -uroot -pP@ssw0rd"
 
-echo "[1] slow query log 활성화"
+echo "[1단계] slow query log 활성화"
 $MYSQL_CMD < 01_enable_slow_query.sql
 
-echo "[2-1] 느린 쿼리 발생"
+echo "[2-1단계] 느린 쿼리 발생"
 $MYSQL_CMD < 02_generate_slow_queries.sql
 
-echo "[2-2] 느린 쿼리 발생"
+echo "[2-2단계] 느린 쿼리 발생"
 $MYSQL_CMD < 02_generate_slow_queries2.sql
 
-echo "[3] pt-query-digest 분석"
+echo "[3단계] pt-query-digest 분석"
 bash analyze_with_ptqd.sh
 
-echo "[4] 정리"
+echo "[4단계] 정리"
 $MYSQL_CMD < 03_cleanup.sql

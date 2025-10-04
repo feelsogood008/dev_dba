eb0866ef53743434f3628c03bb8241d1c8bc899b
008 <39646894+feelsogood008@users.noreply.github.com>
2025-10-04 11:13:58 +0900

    Update cleanup_kill.sh

--- 09_uncommitted_transaction/cleanup_kill.sh
+++ 09_uncommitted_transaction/cleanup_kill.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # cleanup_kill.sh
 # information_schema.innodb_trx에서 trx_mysql_thread_id를 조회해 KILL로 트랜잭션 강제 종료
-# 실행 전에 mysql 접속정보를 확인 (또는 ~/.my.cnf 사용 권장)
 
 MYSQL_USER="root"
-# 보안: 환경에 따라 비밀번호 자동화하면 안 됨. 권장: ~/.my.cnf 에 자격정보 저장
+# 보안: 환경에 따라 비밀번호 자동화하면 안 됨.
 # MYSQL_PWD 환경변수 사용시 주의
 MYSQL_OPTS="-u${MYSQL_USER} -pP@ssw0rd"
 

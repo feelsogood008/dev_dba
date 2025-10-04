92714c783cce3a56133095dd8687fce448c1cbaa
008 <39646894+feelsogood008@users.noreply.github.com>
2025-10-04 20:00:05 +0900

    Update run_all.sh

--- 12_realtime_tuning_checklist/run_all.sh
+++ 12_realtime_tuning_checklist/run_all.sh
@@ -3,18 +3,18 @@ MYSQL_USER="root"
 MYSQL_PWD="P@ssw0rd"
 
 
-echo "[1] 부하 발생 시작"
+echo "[1단계] 부하 발생 시작"
 ./01_generate_load.sh &
 LOAD_PID=$!
 sleep 5
 
-echo "[2] 현재 실행 중인 쿼리 확인"
+echo "[2단계] 현재 실행 중인 쿼리 확인"
 mysql -u$MYSQL_USER -p$MYSQL_PWD < 02_show_processlist.sql
 
-echo "[3] 락 상태 확인"
+echo "[3단계] 락 상태 확인"
 mysql -u$MYSQL_USER -p$MYSQL_PWD < 03_check_locks.sql
 
-echo "[4] InnoDB 상태 확인"
+echo "[4단계] InnoDB 상태 확인"
 mysql -u$MYSQL_USER -p$MYSQL_PWD < 04_check_innodb_status.sql
 
 wait $LOAD_PID

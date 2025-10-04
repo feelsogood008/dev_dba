e7331bda785ae58127d8c7948973ab3d1b077fe3
008 <39646894+feelsogood008@users.noreply.github.com>
2025-10-04 13:36:13 +0900

    Update analyze_with_ptqd.sh

--- 10_slow_query_analysis/analyze_with_ptqd.sh
+++ 10_slow_query_analysis/analyze_with_ptqd.sh
@@ -3,10 +3,10 @@ SLOW_LOG="/var/lib/mysql/mysql-slow.log"
 
 if ! command -v pt-query-digest &> /dev/null; then
     echo "pt-query-digest가 설치되어 있지 않습니다."
-    echo "설치 방법: sudo apt install percona-toolkit (README.md 파일 참고)"
+    echo "설치 방법 : sudo apt install percona-toolkit (README.md 파일 참고)"
     exit 1
 fi
 
 echo "[info-1] Slow query 로그 분석 시작..."
 sudo pt-query-digest $SLOW_LOG > slow_report.txt
-echo "[info-2] 분석 완료: slow_report.txt 확인"
+echo "[info-2] 분석 완료 : slow_report.txt 확인"

f873797e81f32bc155cc9f2074c201ffaaf57f52
008 <39646894+feelsogood008@users.noreply.github.com>
2025-10-05 14:31:33 +0900

    Update run_all.sh

--- 13_safe_ddl_deploy/run_all.sh
+++ 13_safe_ddl_deploy/run_all.sh
@@ -2,7 +2,7 @@
 MYSQL_CMD="mysql -u root -pP@ssword"
 
 echo "[1단계] 테스트 테이블 생성"
-$MYSQL_CMD < 01_create_tables_insert_date.sql
+$MYSQL_CMD < 01_create_tables_insert_data.sql
 
 echo "[2단계] DDL 락 대기 상황 재현"
 echo "세션1에서 긴 SELECT ... FOR UPDATE 실행 후 세션2에서 ALTER TABLE 실행 (02_blocking_query.sql)"

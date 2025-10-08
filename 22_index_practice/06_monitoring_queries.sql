3a63c204338be35bcd985420cfe3d52abc973e62
008 <39646894+feelsogood008@users.noreply.github.com>
2025-10-08 17:20:34 +0900

    Update 06_monitoring_queries.sql

--- 22_index_practice/06_monitoring_queries.sql
+++ 22_index_practice/06_monitoring_queries.sql
@@ -1,13 +1,15 @@
 
 
--- 쿼리 실행 시간 확인
+-- 정보1) 쿼리 실행 시간 확인
 SHOW PROFILES;
 
--- 인덱스 사용 현황
+-- 정보2) 인덱스 사용 현황
 SHOW INDEX FROM orders;
 
 -- 핸들러 통계 (풀스캔 발생 여부 확인)
+--> 쿼리 수행
 SHOW GLOBAL STATUS LIKE 'Handler_read%';
+
 
 -- InnoDB 상태 확인 (잠금, 버퍼 풀 등)
 SHOW ENGINE INNODB STATUS\G
@@ -20,3 +22,4 @@ ORDER BY SUM_TIMER_WAIT DESC LIMIT 10;
 SELECT OBJECT_SCHEMA, OBJECT_NAME, INDEX_NAME, COUNT_STAR
 FROM performance_schema.table_io_waits_summary_by_index_usage
 WHERE INDEX_NAME IS NULL OR COUNT_STAR > 0;
+

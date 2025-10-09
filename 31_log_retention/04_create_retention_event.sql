36b652e5ca543555d8cc062cde076df4546e3563
008 <39646894+feelsogood008@users.noreply.github.com>
2025-10-09 21:43:57 +0900

    Update 04_create_retention_event.sql

--- 31_log_retention/04_create_retention_event.sql
+++ 31_log_retention/04_create_retention_event.sql
@@ -10,26 +10,50 @@ DROP PROCEDURE IF EXISTS rotate_log_partitions;
 DELIMITER //
 CREATE PROCEDURE rotate_log_partitions()
 BEGIN
-  DECLARE pname VARCHAR(16);
-  DECLARE stmt  TEXT;
+  DECLARE pname VARCHAR(32);
+  DECLARE cnt INT DEFAULT 0;
+  DECLARE has_err INT DEFAULT 0;
 
-  -- 3개월 전 월 파티션 이름 계산 (예: p202505)
+  -- 오류 발생시 has_err 를 1로 세팅 (빈 BEGIN/END 대신)
+  DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET has_err = 1;
+
   SET pname = DATE_FORMAT(DATE_SUB(CURDATE(), INTERVAL 3 MONTH), 'p%Y%m');
-  SET stmt  = CONCAT('ALTER TABLE logs.log_events_part DROP PARTITION ', pname);
 
-  -- 파티션 없을 수도 있으니 에러 무시
-  DECLARE CONTINUE HANDLER FOR SQLEXCEPTION BEGIN END;
+  -- 파티션 존재 여부 확인
+  SELECT COUNT(*) INTO cnt
+  FROM information_schema.PARTITIONS
+  WHERE TABLE_SCHEMA = 'logs'
+    AND TABLE_NAME = 'log_events_part'
+    AND PARTITION_NAME = pname;
 
-  PREPARE s FROM stmt;
-  EXECUTE s;
-  DEALLOCATE PREPARE s;
-END//
+  IF cnt = 0 THEN
+    SELECT CONCAT('partition ', pname, ' does not exist') AS info;
+  ELSE
+    -- PREPARE는 세션 변수(@sql) 사용
+    SET @sql = CONCAT('ALTER TABLE `logs`.`log_events_part` DROP PARTITION `', pname, '`');
+    PREPARE dyn FROM @sql;
+    EXECUTE dyn;
+    DEALLOCATE PREPARE dyn;
+
+    IF has_err = 1 THEN
+      SELECT CONCAT('error occurred while dropping partition ', pname) AS error;
+    ELSE
+      SELECT CONCAT('dropped partition ', pname) AS info;
+    END IF;
+  END IF;
+END //
 DELIMITER ;
 
+-- 이벤트 등록 (rotate_log_partitions 실행)
 DROP EVENT IF EXISTS ev_rotate_log_partitions;
 CREATE EVENT ev_rotate_log_partitions
   ON SCHEDULE EVERY 1 DAY STARTS CURRENT_DATE + INTERVAL 1 DAY + INTERVAL 3 HOUR + INTERVAL 30 MINUTE
   DO CALL rotate_log_partitions();
+
+
+-- CALL rotate_log_partitions();
+-- SHOW WARNINGS;
+
 
 -- 참고: 새 월 진입 시 다음달 파티션을 미리 추가하는 작업도 권장
 DROP PROCEDURE IF EXISTS ensure_next_month_partition;
@@ -38,23 +62,53 @@ CREATE PROCEDURE ensure_next_month_partition()
 BEGIN
   DECLARE next_p  VARCHAR(16);
   DECLARE next_v  DATE;
-  DECLARE stmt    TEXT;
-  DECLARE CONTINUE HANDLER FOR SQLEXCEPTION BEGIN END;
+  DECLARE cnt     INT DEFAULT 0;
+  DECLARE has_err INT DEFAULT 0;
 
-  SET next_v = DATE_FORMAT(DATE_ADD(LAST_DAY(CURDATE()), INTERVAL 1 DAY), '%Y-%m-01');
+  -- 예외 발생 시 플래그 설정 (빈 BEGIN END 대신)
+  DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET has_err = 1;
+
+  -- 다음 달 1일 (파티션 경계값)
+  SET next_v = DATE_ADD(LAST_DAY(CURDATE()), INTERVAL 1 DAY);
   SET next_p = DATE_FORMAT(next_v, 'p%Y%m');
 
-  SET @exists := (SELECT COUNT(*) FROM information_schema.partitions
-                  WHERE table_schema='logs' AND table_name='log_events_part' AND partition_name=next_p);
-  IF @exists = 0 THEN
-    SET stmt = CONCAT("ALTER TABLE logs.log_events_part ADD PARTITION (PARTITION ", next_p,
-                      " VALUES LESS THAN ('", DATE_FORMAT(DATE_ADD(next_v, INTERVAL 1 MONTH), '%Y-%m-01'), "'))");
-    PREPARE s FROM stmt; EXECUTE s; DEALLOCATE PREPARE s;
+  -- 파티션 존재 여부 확인
+  SELECT COUNT(*) INTO cnt
+  FROM information_schema.PARTITIONS
+  WHERE TABLE_SCHEMA = 'logs'
+    AND TABLE_NAME = 'log_events_part'
+    AND PARTITION_NAME = next_p;
+
+  IF cnt = 0 THEN
+    -- ADD PARTITION SQL 구성
+    SET @sql = CONCAT(
+      'ALTER TABLE `logs`.`log_events_part` ',
+      'ADD PARTITION (PARTITION `', next_p, '` VALUES LESS THAN (\'',
+      DATE_FORMAT(DATE_ADD(next_v, INTERVAL 1 MONTH), '%Y-%m-01'),
+      '\'))'
+    );
+
+    PREPARE dyn FROM @sql;
+    EXECUTE dyn;
+    DEALLOCATE PREPARE dyn;
+
+    IF has_err = 1 THEN
+      SELECT CONCAT('Error while adding partition ', next_p) AS msg;
+    ELSE
+      SELECT CONCAT('Added partition ', next_p) AS msg;
+    END IF;
+  ELSE
+    SELECT CONCAT('Partition ', next_p, ' already exists') AS msg;
   END IF;
-END//
+END //
 DELIMITER ;
 
+
+-- 이벤트 등록 (ev_ensure_next_month_partition 실행)
 DROP EVENT IF EXISTS ev_ensure_next_month_partition;
 CREATE EVENT ev_ensure_next_month_partition
   ON SCHEDULE EVERY 1 DAY STARTS CURRENT_DATE + INTERVAL 1 DAY + INTERVAL 2 HOUR
   DO CALL ensure_next_month_partition();
+
+-- CALL ensure_next_month_partition();
+-- SHOW WARNINGS;

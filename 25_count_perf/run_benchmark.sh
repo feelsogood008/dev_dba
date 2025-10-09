f98f3bd8d580cb4458894c3cad70b261dc3b0cda
008 <39646894+feelsogood008@users.noreply.github.com>
2025-10-09 13:10:48 +0900

    Update run_benchmark.sh

--- 25_count_perf/run_benchmark.sh
+++ 25_count_perf/run_benchmark.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-MYSQL="mysql -u root -pYOURPASSWORD testdb"
+MYSQL="mysql -u root -pP@ssw0rd demo_db"
 LOGFILE="count_benchmark.log"
 
 echo "===== COUNT(*) 성능 비교 시작 =====" > $LOGFILE
 date >> $LOGFILE
 
-echo -e "\n--- 데이터 준비 ---" >> $LOGFILE
-$MYSQL < 25_create_data.sql >> $LOGFILE 2>&1
-
 echo -e "\n--- 성능 비교 실행 ---" >> $LOGFILE
-$MYSQL < 25_count_test.sql >> $LOGFILE 2>&1
+$MYSQL < 02_count_test.sql >> $LOGFILE 2>&1
 
 echo -e "\n===== COUNT(*) 성능 비교 종료 =====" >> $LOGFILE
 date >> $LOGFILE

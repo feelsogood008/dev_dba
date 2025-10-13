217eaf9c3f34a2ed720b8a6c73ee51a34a321e94
008 <39646894+feelsogood008@users.noreply.github.com>
2025-10-13 21:35:11 +0900

    Update run_all.sh

--- 18_slow_query_log/run_all.sh
+++ 18_slow_query_log/run_all.sh
@@ -1,17 +1,23 @@
 
 #!/bin/bash
-MYSQL_USER="root"
-MYSQL_PWD="P@ssw0rd"
+MYSQL_CMD="mysql -u root -pP@ssw0rd"
 
 
-echo "[1] 슬로우쿼리 로그 활성화"
-mysql -u$MYSQL_USER -p$MYSQL_PWD < 01_enable_slow_log.sql
+echo "[1단계] 슬로우쿼리 로그 활성화"
+$MYSQL_CMD < 01_enable_slow_log.sql
 
-echo "[2] 느린 쿼리 실행"
-mysql -u$MYSQL_USER -p$MYSQL_PWD < 02_generate_slow_queries.sql
+echo "[2단계] 테이블 생성과 데이터 입력"
+$MYSQL_CMD < 02_create_tables_insert_data.sql
 
-echo "[3] mysqldumpslow 분석"
-./03_analyze_with_mysqldumpslow.sh
+echo "[3단계] 다양한 형태의 느린 쿼리 실행"
+$MYSQL_CMD < 03_generate_slow_queries.sql
 
-echo "[4] pt-query-digest 분석"
-./04_analyze_with_pt_digest.sh
+echo "[4단계] mysqldumpslow 분석"
+bash ./04_analyze_with_mysqldumpslow.sh
+
+echo "[5단계] pt-query-digest 분석"
+bash ./05_analyze_with_pt_digest.sh
+
+echo "[6단계] 리소스 정리"
+$MYSQL_CMD < 06_cleanup.sql
+

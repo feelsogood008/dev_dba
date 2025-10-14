e5406ac270a66c7ccd08f04025de810d73e9bffd
008 <39646894+feelsogood008@users.noreply.github.com>
2025-10-14 22:12:52 +0900

    Update 05_analyze_with_pt_digest.sh

--- 18_slow_query_log/05_analyze_with_pt_digest.sh
+++ 18_slow_query_log/05_analyze_with_pt_digest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 로그파일의 경로를 적절하게 수정한다.
-LOG_FILE="/var/log/mysql/slow.log"
+LOG_FILE="/var/log/mysql/mysql-slow.log"
 
 echo "[pt-query-digest 분석]"
 pt-query-digest $LOG_FILE > slow_digest_report.txt

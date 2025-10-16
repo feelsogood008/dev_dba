e201cb8407bf33bb0df9b322183d119b639ed223
008 <39646894+feelsogood008@users.noreply.github.com>
2025-10-16 09:50:31 +0900

    Update 04_analyze_with_mysqldumpslow.sh

--- 18_slow_query_log/04_analyze_with_mysqldumpslow.sh
+++ 18_slow_query_log/04_analyze_with_mysqldumpslow.sh
@@ -3,4 +3,9 @@
 LOG_FILE="/var/log/mysql/mysql-slow.log"
 
 echo "[mysqldumpslow 분석]"
+# 쿼리 실행 빈도(count) 기준 정렬로 상위 10개
 mysqldumpslow -s c -t 10 $LOG_FILE
+
+# 쿼리 실행 시간(query time) 기준 정렬로 상위 10개
+# mysqldumpslow -s t -t 10 $LOG_FILE
+

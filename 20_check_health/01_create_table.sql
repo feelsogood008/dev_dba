96105b958dd7ccf61178afd9e5ded8d10628acaf
008 <39646894+feelsogood008@users.noreply.github.com>
2025-10-17 17:20:33 +0900

    Update 01_create_table.sql

--- 20_check_health/01_create_table.sql
+++ 20_check_health/01_create_table.sql
@@ -1,7 +1,8 @@
 -- 20_check_health.sql
 -- 점검 데이터 저장용 DB와 테이블 생성
 
-CREATE DATABASE IF NOT EXISTS demo_db;
+CREATE DATABASE IF NOT EXISTS demo_db DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 
 USE demo_db;
 
@@ -17,3 +18,4 @@ CREATE TABLE IF NOT EXISTS check_history (
 
 -- 확인용 샘플 조회
 SELECT * FROM check_history ORDER BY id DESC LIMIT 5;
+

5314edb9f70f45a5b5bfa788153e0278da2b2530
008 <39646894+feelsogood008@users.noreply.github.com>
2025-10-18 12:59:55 +0900

    Update 05_verify_restore.sh

--- 19_backup_strategy/05_verify_restore.sh
+++ 19_backup_strategy/05_verify_restore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 복구 검증 전략
+# 복구 검증
 # 사용법: ./05_verify_restore.sh ./backup_strategy/xtrabackup_full/full_xxxxx
 
 BACKUP_DIR=$1
@@ -15,7 +15,7 @@ systemctl stop mysql
 
 echo "[검증] 기존 데이터 삭제 후 복원"
 rm -rf $DATADIR/*
-xtrabackup --copy-back --target-dir=$BACKUP_DIR
+sudo xtrabackup --copy-back --target-dir=$BACKUP_DIR
 chown -R mysql:mysql $DATADIR
 
 echo "[검증] MySQL 서비스 시작"

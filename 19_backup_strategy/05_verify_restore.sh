0be0c1f9077bdaf17e5645976cff5526cdf042c0
008 <39646894+feelsogood008@users.noreply.github.com>
2025-10-18 21:53:00 +0900

    Update 05_verify_restore.sh

--- 19_backup_strategy/05_verify_restore.sh
+++ 19_backup_strategy/05_verify_restore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 복구 검증
+# 복구 검증 (풀백업 파일을 이용한 복원)
 # 사용법: ./05_verify_restore.sh ./backup_strategy/xtrabackup_full/full_xxxxx
 
 BACKUP_DIR=$1
@@ -14,17 +14,17 @@ fi
 echo "[백업 준비] prepare(redo 로그 적용)"
 sudo xtrabackup --prepare --target-dir=$BACKUP_DIR
 
-
 echo "[검증] MySQL 서비스 중지"
 sudo systemctl stop mysql
 
 echo "[검증] 기존 데이터 삭제 후 복원"
-sudo rm -rf $DATADIR/* $DATADIR/.[!.]*
-sudo mkdir -p /var/lib/mysql
-sudo chown mysql:mysql /var/lib/mysql
-
+sudo rm -rf $DATADIR/* 
 sudo xtrabackup --copy-back --target-dir=$BACKUP_DIR --datadir=$DATADIR
 sudo chown -R mysql:mysql $DATADIR
+
+# xtrabackup: Can't create/write to file '/var/log/mysql/mysql-bin.000xxx' (OS errno 17 - File exists) 와 같은
+# 에러 발생시에는 sudo xtrabackup --no-defaults --copy-back --target-dir=$BACKUP_DIR --datadir=$DATADIR 처럼
+# --no-defaults 옵션을 추가한다.
 
 echo "[검증] MySQL 서비스 시작"
 sudo systemctl start mysql
